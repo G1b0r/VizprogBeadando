65001e3cdda8528c36b3ad9f068a97a48ee9bc0b
forgacsg <forgacsg@FGP-LAPTOP>
2025-04-24 21:03:06 +0200

    Add project files.

---
+++ project-library/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using project_library.Core;
+using project_library.MVVM.View;
+
+namespace project_library.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand LoginViewCommand { get; set; }
+        public RelayCommand RegisterViewCommand { get; set; }
+
+        public LoginViewModel LoginVM { get; set; }
+
+        public RegisterViewModel RegisterVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value; 
+                OnPropertyChanged(); 
+            }
+        }
+
+
+        public MainViewModel() 
+        {
+            LoginVM = new LoginViewModel();
+            RegisterVM = new RegisterViewModel();
+
+            CurrentView = LoginVM;
+
+            LoginViewCommand = new RelayCommand(o => { CurrentView = LoginVM; });
+            RegisterViewCommand = new RelayCommand(o => { CurrentView = RegisterVM; });
+        }
+
+    }
+}
---
+++ project-library/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace project_library
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsDarkTheme { get; set; }
+        private readonly PaletteHelper paletteHelper = new PaletteHelper();   
+        private void toggleTheme(object sender, RoutedEventArgs e)
+        {
+            Theme theme = paletteHelper.GetTheme();
+
+            if (IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                IsDarkTheme = false;
+                theme.SetBaseTheme(BaseTheme.Light);
+            } 
+            else
+            {
+                IsDarkTheme = true;
+                theme.SetBaseTheme(BaseTheme.Dark);
+            }
+            paletteHelper.SetTheme(theme);
+        }
+
+        private void exitApp(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+
+    }
+}
