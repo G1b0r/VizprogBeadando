1c558e3d16b7ae16e05ed03211fbb04fcc811444
G1b0r <forgacs.gabor2004@gmail.com>
2025-04-26 10:04:36 +0200

    homepage

--- project-library/MVVM/ViewModel/MainViewModel.cs
+++ project-library/MVVM/ViewModel/MainViewModel.cs
@@ -27,12 +27,14 @@ namespace project_library.MVVM.ViewModel
 
         public ICommand LoginViewCommand { get; }
         public ICommand RegisterViewCommand { get; }
+        public ICommand HomeViewCommand { get; }
 
         public MainViewModel()
         {
             // Initialize commands
             LoginViewCommand = new RelayCommand(o => CurrentView = new LoginViewModel());
             RegisterViewCommand = new RelayCommand(o => CurrentView = new RegisterViewModel());
+            HomeViewCommand = new RelayCommand(o => CurrentView = new HomeViewModel());
 
             // Set default view
             CurrentView = new LoginViewModel();
