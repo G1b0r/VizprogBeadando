7c38c1b582a6d79ddf2ceeb0f620bfc4650edc8c
Sándor <doka.sanyi2001@gmail.com>
2025-05-01 19:13:28 +0200

    Delete me from life

--- project-library/MVVM/View/AdminView.xaml.cs
+++ project-library/MVVM/View/AdminView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using project_library.MVVM.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace project_library.MVVM.View
         public AdminView()
         {
             InitializeComponent();
+            DataContext = new AdminViewModel();
         }
 
         private void addBookBtn_Click(object sender, RoutedEventArgs e)
@@ -33,5 +35,7 @@ namespace project_library.MVVM.View
             // Show the window as a dialog
             addBookWindow.ShowDialog();
         }
+
+
     }
 }
--- project-library/MVVM/ViewModel/AdminViewModel.cs
+++ project-library/MVVM/ViewModel/AdminViewModel.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Library;
+using project_library.Core;
 
 namespace project_library.MVVM.ViewModel
 {
-    class AdminViewModel
+    public class AdminViewModel : ObservableObject
     {
+        private string _bookId;
+
+        public string BookId
+        {
+            get => _bookId;
+            set
+            {
+                _bookId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand DeleteBookCommand { get; }
+
+        public AdminViewModel()
+        {
+            DeleteBookCommand = new RelayCommand(DeleteBook);
+        }
+
+        private void DeleteBook(object parameter)
+        {
+            if (int.TryParse(BookId, out int bookId))
+            {
+                using (var dbContext = new LibraryDbContext())
+                {
+                    var book = dbContext.Books.Find(bookId);
+                    if (book != null)
+                    {
+                        book.deleted = true;
+                        dbContext.SaveChanges();
+                        MessageBox.Show($"Book with ID {bookId} has been marked as deleted.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"No book found with ID {bookId}.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid numeric Book ID.", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
     }
 }
